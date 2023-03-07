701354497ffc95000a4708a0cb30fafb6075ff7d
farukgoc <farukgoc46@hotmail.com>
2023-03-07 14:45:53 -0500

    Omer's Second Commit

--- src/test/java/com/cloud/step_definitions/US92_StepDefs_OFG.java
+++ src/test/java/com/cloud/step_definitions/US92_StepDefs_OFG.java
@@ -17,32 +17,32 @@ public class US92_StepDefs_OFG extends BasePage {
     LoginPage loginPage = new LoginPage();
     FilesPage_OFG filesPage_ofg = new FilesPage_OFG();
 
-    @Given("User is already logged into the Ceallo - OFG")
+    @Given("User is already logged into the Ceallo")
     public void user_is_already_logged_into_the_ceallo() {
         loginPage.login(ConfigurationReader.getProperty("username"), ConfigurationReader.getProperty("password"));
     }
 
-    @Given("user on the dashboard page - OFG")
+    @Given("user on the dashboard page")
     public void user_on_the_dashboard_page() {
         BrowserUtils.verifyElementDisplayed(logoIcon);
     }
 
-    @When("the user clicks the Files module - OFG")
+    @When("the user clicks the Files module")
     public void the_user_clicks_the_files_module() {
         filesOption.click();
     }
 
-    @Then("verify the page title is {string} - OFG")
+    @Then("verify the page title is {string}")
     public void verify_the_page_title_is(String expectedTitle) {
         BrowserUtils.verifyTitle(expectedTitle);
     }
 
-    @When("user click the top-left checkbox of the table - OFG")
+    @When("user click the top-left checkbox of the table")
     public void user_click_the_top_left_checkbox_of_the_table() {
         filesPage_ofg.selectAllCheckbox.click();
     }
 
-    @Then("verify all the files are selected - OFG")
+    @Then("verify all the files are selected")
     public void verify_all_the_files_are_selected() {
         for (WebElement each : filesPage_ofg.availableCheckboxes) {
             Assert.assertTrue(each.isSelected());
