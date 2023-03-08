9e0e3640e786593b84d7e53adf83f7d8f7842054
Try2Teachme <regboxformail@gmail.com>
2023-03-07 21:23:32 -0500

    Config.prop changed

---
+++ src/test/java/com/cloud/step_definitions/FavoritesStep_definitions.java
@@ -0,0 +1,35 @@
+package com.cloud.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class FavoritesStep_definitions {
+
+
+    @Given("user on the dashboard page")
+    public void user_on_the_dashboard_page() {
+
+    }
+    @When("the user clicks the {string} module")
+    public void the_user_clicks_the_module(String string) {
+
+    }
+    @When("the user clicks action-icon from any file on the page")
+    public void the_user_clicks_action_icon_from_any_file_on_the_page() {
+
+    }
+    @When("user choose the {string} option")
+    public void user_choose_the_option(String string) {
+
+    }
+    @When("user click the {string} sub-module on the left side")
+    public void user_click_the_sub_module_on_the_left_side(String string) {
+
+    }
+    @Then("Verify the chosen file is listed on the table")
+    public void verify_the_chosen_file_is_listed_on_the_table() {
+
+    }
+
+}
