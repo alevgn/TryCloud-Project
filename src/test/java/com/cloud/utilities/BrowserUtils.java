29280e3d499eadcbbdb65fed386d075319b045c0
veronica_ilie2001@yhaoo.com <veronica_ilie2001@yahoo.com>
2023-03-10 17:43:39 -0600

    VS's commit

--- src/test/java/com/cloud/utilities/BrowserUtils.java
+++ src/test/java/com/cloud/utilities/BrowserUtils.java
@@ -1,17 +1,20 @@
-package com.cloud.utilities;
 
-import org.junit.Assert;
-import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+
+        package com.cloud.utilities;
+
+        import org.junit.Assert;
+        import org.openqa.selenium.*;
+        import org.openqa.selenium.interactions.Actions;
+        import org.openqa.selenium.support.ui.ExpectedCondition;
+        import org.openqa.selenium.support.ui.ExpectedConditions;
+        import org.openqa.selenium.support.ui.Select;
+        import org.openqa.selenium.support.ui.WebDriverWait;
+
+        import java.time.Duration;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.Set;
 
 public class BrowserUtils {
 
