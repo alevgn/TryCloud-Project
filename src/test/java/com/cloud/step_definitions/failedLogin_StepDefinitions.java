3093d806ca99fba2c499c844efbb1d7fffb08ef7
ligostaevaa <ligostaevaa17@gmail.com>
2023-03-10 10:07:45 -0600

    failedLogin.feature file and failedLogin_StepDefinitions are updated

--- src/test/java/com/cloud/step_definitions/failedLogin_StepDefinitions.java
+++ src/test/java/com/cloud/step_definitions/failedLogin_StepDefinitions.java
@@ -1,7 +1,43 @@
 package com.cloud.step_definitions;
 
+import com.cloud.pages.LoginPage;
+import com.cloud.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
 public class failedLogin_StepDefinitions {
 
+    @Given("user is on the login page projectURL")
+    public void user_is_on_the_login_page_project_url() {
+        Driver.getDriver().get("https://qa.ceallo.com/index.php/login");
+    }
+
+    LoginPage loginPage = new LoginPage();
+
+    @When("user enters invalid username {string}")
+    public void user_enters_invalid_and(String invalidUsername) {
+        loginPage.userName.sendKeys(invalidUsername);
+    }
+
+    @And("user enters invalid password {string}")
+    public void userEntersInvalidPassword(String invalidPassword) {
+        loginPage.password.sendKeys(invalidPassword);
+    }
+
+    @When("user clicks the login button")
+    public void user_clicks_the_login_button() {
+        loginPage.submit.click();
+    }
+
+
+
+    @Then("verify {string} message should be displayed")
+    public void verify_message_should_be_displayed(String expectedWarningMessage) {
+        Assert.assertEquals(loginPage.warningMessage.getText(), expectedWarningMessage);
+    }
 
 
 
--- src/test/resources/features/failedLogin.feature
+++ src/test/resources/features/failedLogin.feature
@@ -3,12 +3,13 @@ Feature: As a user, I should not be able to login with invalid credentials.
 
   Scenario Outline: Verify user login fail with invalid credentials
     Given user is on the login page projectURL
-    When user enters invalid "<username>" and "<password>"
+    When user enters invalid username "<invalidUsername>"
+    And user enters invalid password "<invalidPassword>"
     And user clicks the login button
-    Then verify "<message>" message should be displayed
+    Then verify "<expectedWarningMessage>" message should be displayed
 
     Examples:
-      | username | password | message                     |
-      | MaryK4   | 33       | Wrong username or password. |
-      | 4        | L        | Wrong username or password. |
-      | .        | 123!     | Wrong username or password. |
+      | invalidUsername | invalidPassword | expectedWarningMessage      |
+      | MaryK4          | 33              | Wrong username or password. |
+      | 4               | L               | Wrong username or password. |
+      | .               | 123!            | Wrong username or password. |
